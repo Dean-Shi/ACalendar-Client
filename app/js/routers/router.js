cadf196c1f3e9379ec6991888ef80399ada08908
Dean Shi <dean@accsc.im>
2014-03-24 19:51:11 -0300

    Changed: Removed unused code

--- app/js/routers/router.js
+++ app/js/routers/router.js
@@ -1,3 +1,6 @@
+/*
+ *  Router
+ */
 define([
     'jquery',
     'underscore',
@@ -10,8 +13,7 @@ define([
         routes: {
             // Default
             "*actions": "index",
-            "editor": "editor",
-            "catalog": ""
+            "editor": "editor"
         },
 
         initialize: function () {
@@ -25,28 +27,4 @@ define([
             new EventEditorView();
         }
     });
-
-    // var initialize = function(){
-    //     var app_router = new AppRouter;
-    //     app_router.on('showProjects', function(){
-    //       // Call render on the module we loaded in via the dependency array
-    //       // 'views/projects/list'
-    //       var projectListView = new ProjectListView();
-    //       projectListView.render();
-    //     });
-    //       // As above, call render on our loaded module
-    //       // 'views/users/list'
-    //     app_router.on('showUsers', function(){
-    //       var userListView = new UserListView();
-    //       userListView.render();
-    //     });
-    //     app_router.on('defaultAction', function(actions){
-    //       // We have no matching route, lets just log what the URL was
-    //       console.log('No route:', actions);
-    //     });
-    //     Backbone.history.start();
-    // };
-    // return {
-    //     initialize: initialize
-    // };
 });
